9dd3c4224f69968090a08fe5dce67742a37d59fd
Aleksandar Maksimovic <aleksandar.maksimovic2608@gmail.com>
2018-02-12 22:01:50 +0100

    First commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import '/.App.css';
+import {FromGroup, FormControl, InputGroup, Glyphicon} from 'react-bootstrap';
+
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+            
+            </div>
+        )
+    }
+}
+
+export default App;
