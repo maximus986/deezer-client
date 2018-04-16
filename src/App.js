e1b7ed540385039fa29e65c6e69470bda94cbe0b
Aleksandar Maksimovic <aleksandar.maksimovic2608@gmail.com>
2018-04-16 22:45:39 +0200

    Create the App component, add boostrap features and implement basic styles

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import React, {Component} from 'react';
-import '/.App.css';
-import {FromGroup, FormControl, InputGroup, Glyphicon} from 'react-bootstrap';
+import './App.css';
+import {Form, FormGroup, FormControl, InputGroup, Glyphicon} from 'react-bootstrap';
 
 class App extends Component {
     render() {
         return (
             <div className="App">
-            
+                <div className="App-title">Music Master</div>
+                    <FormGroup>
+                        <InputGroup>
+                            <FormControl type="text" placeholder="Search for an artist..." /> 
+                            <InputGroup.Addon>
+                                <Glyphicon glyph="search"></Glyphicon>
+                            </InputGroup.Addon>
+                        </InputGroup>
+                    </FormGroup>
+                <div className="Profile">  
+                    <div>Artist Picture</div>
+                    <div>Artist Name</div>
+                </div>
+                <div className="Gallery">
+                    Gallery
+                </div>
             </div>
         )
     }
