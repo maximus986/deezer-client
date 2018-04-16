648f7da4cfa62bd6c078031a3730f81740df3515
Aleksandar Maksimovic <aleksandar.maksimovic2608@gmail.com>
2018-04-16 23:02:03 +0200

    Implement search method

--- src/App.js
+++ src/App.js
@@ -1,16 +1,34 @@
 import React, {Component} from 'react';
 import './App.css';
-import {Form, FormGroup, FormControl, InputGroup, Glyphicon} from 'react-bootstrap';
+import {FormGroup, FormControl, InputGroup, Glyphicon} from 'react-bootstrap';
 
 class App extends Component {
+
+    state = {
+        query: '' 
+    }
+
+    search() {
+        console.log("this.state", this.state); 
+    }
+        
     render() {
         return (
             <div className="App">
                 <div className="App-title">Music Master</div>
                     <FormGroup>
                         <InputGroup>
-                            <FormControl type="text" placeholder="Search for an artist..." /> 
-                            <InputGroup.Addon>
+                            <FormControl 
+                                type="text" 
+                                placeholder="Search for an artist..." 
+                                value={this.state.query}
+                                onChange={event => this.setState({query: event.target.value})} 
+                                onKeyPress={event => {
+                                    if(event.key === "Enter") {
+                                        this.search();
+                                    }
+                                }}/> 
+                            <InputGroup.Addon onClick={() => this.search()}>
                                 <Glyphicon glyph="search"></Glyphicon>
                             </InputGroup.Addon>
                         </InputGroup>
