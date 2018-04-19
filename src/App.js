e5ae55b960b04b631e4aea316c077da4b996e5fb
Aleksandar Maksimovic <aleksandar.maksimovic2608@gmail.com>
2018-04-19 23:18:35 +0200

    Fetch data from Deezer API

--- src/App.js
+++ src/App.js
@@ -10,9 +10,14 @@ class App extends Component {
 
     search() {
         console.log("this.state", this.state); 
-        const BASE_URL = 'https://api.spotify.com/v1/search?';
-        const FETCH_URL = `${BASE_URL}q=${this.state.query}&type=artist&limit=1`;
+        const BASE_URL = 'https://cors-anywhere.herokuapp.com/https://api.deezer.com/search?';
+        const FETCH_URL = `${BASE_URL}q=${this.state.query}`;
             console.log("FETCH_URL", FETCH_URL);
+            fetch(FETCH_URL, {
+                method: 'GET'
+            })
+            .then(response =>response.json())
+            .then(json => console.log(json));
         }
     
         
